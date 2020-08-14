0c9c8d2c7a95236bf3e5668dd6c2bbf40b6df0aa
Victor Nagahama <victorh.nagahama@gmail.com>
2020-08-14 16:21:34 -0300

    fix: 🐛 file format correction in download
    ✅ Closes: #5

--- aula2/exercicios/functions.sh
+++ aula2/exercicios/functions.sh
@@ -4,9 +4,9 @@
 function download_datasets {
     if [[ -d "$1" ]]; then
         echo "Baixando arquivo de 2006"
-        wget -q 'https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/HG7NV7/EPIFFT' -O "${1}/2006.tar.bz2"
+        wget -q 'https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/HG7NV7/EPIFFT' -O "${1}/2006.csv.bz2"
         echo "Baixando arquivo de 2007"
-        wget -q 'https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/HG7NV7/2BHLWK' -O "${1}/2007.tar.bz2"
+        wget -q 'https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/HG7NV7/2BHLWK' -O "${1}/2007.csv.bz2"
         exit 0
     else
         echo "Destino do download não existe"
