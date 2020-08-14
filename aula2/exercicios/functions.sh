6b001c9d392020a54ee8bcb1d4df252099b1eabf
Victor Nagahama <victorh.nagahama@gmail.com>
2020-08-14 16:34:33 -0300

    feat: 🎸 uncompress file
    ✅ Closes: #1

--- aula2/exercicios/functions.sh
+++ aula2/exercicios/functions.sh
@@ -7,6 +7,16 @@ function download_datasets {
         wget -q 'https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/HG7NV7/EPIFFT' -O "${1}/2006.csv.bz2"
         echo "Baixando arquivo de 2007"
         wget -q 'https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/HG7NV7/2BHLWK' -O "${1}/2007.csv.bz2"
+
+        if [[ ! -f "${1}/2006.csv" ]]; then
+            echo "Descompactando arquivo de 2006"
+            bzip2 -dk "${1}/2006.csv.bz2"
+        fi
+
+        if [[ ! -f "${1}/2007.csv" ]]; then
+            echo "Descompactando arquivo de 2007"
+            bzip2 -dk "${1}/2007.csv.bz2"
+        fi
         exit 0
     else
         echo "Destino do download não existe"
