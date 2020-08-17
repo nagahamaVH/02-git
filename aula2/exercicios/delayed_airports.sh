c5cf97b1242e921561964d3407d0bd451257e1c1
Victor Nagahama <victorh.nagahama@gmail.com>
2020-08-17 15:24:17 -0300

    feat: 🎸 list of airports with delayed flights
    ✅ Closes: #4

---
+++ aula2/exercicios/delayed_airports.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# return list of airports with delayed flights
+if [[ -f $1 ]]; then
+    origin=$(awk -F, '{ if($15 > 0) {print} }' $1 | awk -F, '{print $17}' | sort | uniq)
+    dest=$(awk -F, '{ if($16 > 0) {print} }' $1 | awk -F, '{print $18}' | sort | uniq)
+
+    airports=$(echo "${origin}${dest}" | sort | uniq)
+
+    echo "Aeroportos com voos atrasados:"
+
+    for air in ${airports[@]}
+    do
+        # Create airport quoted
+        air_quoted='"'
+        air_quoted+="$air"
+        air_quoted+='"'
+
+        airports_name=$(awk -F, -v var="$air_quoted" '{if ($1 == var) {print} }' ./data/airports.csv)
+
+        echo "$airports_name"
+    done
+else
+    echo "O arquivo não existe"
+fi
